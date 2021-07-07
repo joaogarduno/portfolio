a89290a3e3decfb251fc33f972ba34fecc1d8ea9
Joao Garduño <joao.garduno@gmail.com>
2021-07-07 12:58:52 -0500

    Estructura 3 - Primera version del navMenu, todos los cambios aplicados al menu

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const burger = document.querySelector('.nav__burger');
+    const nav = document.querySelector('.nav__menu--ul')
+
+    burger.addEventListener('click', () => {
+        // TOGGLE NAV
+        nav.classList.toggle('nav-active');
+        // BURGER ANIMATION
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
